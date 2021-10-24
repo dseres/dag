8b00b9d124fcba0c6daa087c64d55a6df12424a8
David Seres <dseres001@gmail.com>
2021-10-24 14:16:22 +0200

    comments fixed

--- src/dag_cr.cr
+++ src/dag_cr.cr
@@ -10,25 +10,15 @@ module DagCr
   #
   # Example:
   # ```
-  # dag = )Graph(Int32).new
-  # dag.add(4)
-  # dag.add(9)
+  # dag = Graph(Int32).new
   # dag.add(1)
   # dag.add(2)
   # dag.add(3)
+  # dag.add(4)
   # dag.add(5)
-  # dag.add(6)
-  # dag.add(7)
-  # dag.add(8)
   # dag.add_edge(1, 3)
-  # dag.add_edge(5, 6)
-  # dag.add_edge(5, 7)
-  # dag.add_edge(6, 4)
-  #
-  # dag.keys            # => eq([9, 1, 3, 2, 5, 6, 4, 7, 8])
-  # dag.successors(5)   # => [6, 7]
-  # dag.predecessors(4) # => [6]
-  # dag[3]              # => nil
+  # dag.add_edge(1, 2)
+  # dag.add_edge(3, 4)
   # ```
   class Graph(V)
     include Enumerable(V)

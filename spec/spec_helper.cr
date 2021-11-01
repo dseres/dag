9ab45253249ce724b7a8035f41d9cde6c6052b6a
dseres <dseres001@gmail.com>
2021-11-01 19:03:17 +0000

    examples in doc comments are revised

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -2,7 +2,7 @@ require "spec"
 require "../src/dag"
 
 def create_test_graph
-  dag = Graph(Int32).new
+  dag = Dag::Graph(Int32).new
   (1...10).each { |i| dag.add i }
   dag.add_edge({1, 3}, {5, 9}, {8, 7}, {8, 6}, {6, 4}, {4, 3}, {4, 7})
   dag
